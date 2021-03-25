c4be94d1f785c7d0d42c2d62fe6ece68bdd31572
OAndell <Oscar@Andell.eu>
2021-03-25 20:21:18 +0100

    mobile improvements

--- src/app/main-page/profile-card/profile-card.component.ts
+++ src/app/main-page/profile-card/profile-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'profile-card',
   templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.css']
+  styleUrls: ['./profile-card.component.scss']
 })
 export class ProfileCardComponent {
 
